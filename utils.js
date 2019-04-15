e27cc894c693ab80f4391c1f4dd2339e26743433
Sam Thorogood <sam.thorogood@gmail.com>
2019-04-15 16:19:33 +1000

    release on Mon 15 Apr 2019 16:19:33 AEST

--- utils.js
+++ utils.js
@@ -1,12 +1,15 @@
 
-async function createWebAssembly(path, importObject) {
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+export async function createWebAssembly(path, importObject) {
   const bytes = await window.fetch(path).then(x => x.arrayBuffer());
   const object = await WebAssembly.instantiate(bytes, importObject);
   object.importObject = importObject;
   return object;
 }
 
-function encodeToArray(s, array) {
+export function encodeToArray(s, array) {
   let i = 0;
   for (let si = 0; si < s.length; ++si) {
     const c = s.charCodeAt(si);
@@ -18,10 +21,23 @@ function encodeToArray(s, array) {
   array[i++] = 0;
 }
 
-function copyToMemory(memory, s) {
-  const array = new Uint8Array(memory.buffer);
-  const startAt = array.length - ((s.length + 1) * 4);
-  const writeTo = new Uint8Array(memory.buffer, startAt);
-  encodeToArray(s, writeTo);
-  return startAt;
+export class CodeView {
+  constructor(memory, s) {
+    const array = new Uint8Array(memory.buffer);
+
+    const bytes = encoder.encode(s);
+    const startAt = array.length - (bytes.length + 1);
+
+    const view = array.subarray(startAt);
+    view.set(bytes);
+    array[array.length - 1] = 0;  // EOF
+
+    this._view = view;
+    this.at = startAt;
+  }
+
+  read(at, len) {
+    const backing = this._view.slice(at, at + len);
+    return decoder.decode(backing);
+  }
 }
